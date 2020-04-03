a90ae4a3fb9664a4511f2ef5c8674bac7e0376d3
Hemant Hari <hemant.hari@gmail.com>
2020-04-03 20:22:54 +0100

    missing things

---
+++ app.js
@@ -0,0 +1,25 @@
+"use strict"
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cors = require('cors');
+var botoxic = require('./bot')
+
+var app = express();
+
+//Middleware
+app.use(cors({credentials: true, origin: true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Static file serving
+app.use(express.static('public'));
+
+app.get('/', entryPoint);
+function entryPoint(req, res){
+  console.log("here I am buddy boy");
+  res.sendFile('public/app.html');
+}
+
+app.listen(8000, () => console.log('listening on port 8000'));
